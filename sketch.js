2e57ac48362b8b61f7d6ea1d4ff71d1204cd66e9
Mehekdeshapde <74087081+Mehekdeshapde@users.noreply.github.com>
2021-01-03 10:03:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+recipe for brownies:
+1.5 tbsp ground flax + 3 tbsp water, whisked
+3/4 cup all-purpose flour
+1.5 cups almond flour
+1/2 cup cocoa powder
+1/2 tsp kosher salt
+1/4 tsp baking soda
+1/4 cup + 2 tbsp  butter
+1/2 cup + 1/4 cup chocolate, divided
+1 cup + 1/4 cup organic cane sugar 
+1/4 cup + 2 tbsp almond milk
+1 tsp vanilla
+1/2 cup finely chopped walnuts( optional)
+
+1. In a large bowl whisk together all the dry ingredients.
+2.melt the chocolate and butter together.
+3.Pour wet mixture over dry mixture.
+4. put in the over for 30 min at 350F.
+
